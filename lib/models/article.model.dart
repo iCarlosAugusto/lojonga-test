4598ba2a8f231cfcdb5c525dd6e679f17712c727
iCarlosAugusto <augusto.dev@proton.me>
2024-01-11 22:53:17 -0300

    feat: :sparkles: new model

--- lib/models/article.model.dart
+++ lib/models/article.model.dart
@@ -1,23 +1,33 @@
 class ArticleModel {
-  String id;
-  String text;
+  int id;
   String title;
-  String imageUrl;
-  String author_name;
-  String url;
-  int premium;
-  int order;
+  String text;
   String image;
 
   ArticleModel({
     required this.id,
-    required this.text,
     required this.title,
-    required this.imageUrl,
-    required this.author_name,
-    required this.url,
-    required this.premium,
-    required this.order,
-    required this.image
+    required this.text,
+    required this.image,
   });
+
+  // Método para converter a instância da classe em um mapa JSON
+  Map<String, dynamic> toJSON() {
+    return {
+      'id': id,
+      'title': title,
+      'text': text,
+      'image': image,
+    };
+  }
+
+  // Método para criar uma instância da classe a partir de um mapa JSON
+  factory ArticleModel.fromJSON(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['id'],
+      title: json['title'],
+      text: json['text'],
+      image: json['image'],
+    );
+  }
 }
